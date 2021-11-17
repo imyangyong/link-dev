129d6e55f48350a1a3ec39b0b085814ded7d6f2b
杨勇 <1994101600@qq.com>
2021-11-17 10:20:03 +0800

    🔥 chore: remove unused code

--- src/link-dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var process_1 = require("process");
-var commander_1 = require("commander");
-var core_1 = require("./core");
-var program = new commander_1.Command();
-program.option('-conf, --config <path>', 'config file path');
-program.parse(process_1["default"].argv);
-core_1["default"](program);
